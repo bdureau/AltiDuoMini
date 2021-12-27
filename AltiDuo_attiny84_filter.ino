1315b6d3c3d72748646042b1b8af00d7e8c727d8
Bear altimeters <boris.dureau@neuf.fr>
2021-12-27 15:14:38 +0100

    fixed apogee

--- AltiDuo_attiny84_filter.ino
+++ AltiDuo_attiny84_filter.ino
@@ -1,11 +1,11 @@
 /*
-  Model Rocket dual deployment altimeter Ver 1.0
+  Model Rocket dual deployment altimeter Ver 1.5
  Copyright Boris du Reau 2012-2013
  
  This is using a BMP085 presure sensor and an Attiny 84
  The following should fire the main at apogee if it is at least 50m above ground of the launch site
  
-  
+ 
  For the BMP085 pressure sensor
  Connect VCC of the BMP085 sensor to 5.0V! make sure that you are using the 5V sensor (GY-65 model)
  Connect GND to Ground
@@ -41,18 +41,18 @@ long lastAltitude;
 boolean apogeeHasFired =false;
 boolean mainHasFired=false;
 //nbr of measures to do so that we are sure that apogee has been reached 
-unsigned long measures;
+unsigned long measures=5;
 unsigned long mainDeployAltitude;
 
 /* TODO: those need to be changed for the attiny 84*/
-const int pinAltitude1 = 8;
-const int pinAltitude2 = 7;
+const int pinAltitude1 = 3;//8;
+const int pinAltitude2 = 2;//7;
 
-const int pinApogee = 10;
-const int pinMain = 0;
-const int pinApogeeContinuity = 9;
-const int pinMainContinuity = 2;
-const int pinSpeaker = 1;
+const int pinApogee = 0;//10;
+const int pinMain = 10;//0;
+const int pinApogeeContinuity = 1;
+const int pinMainContinuity = 8;
+const int pinSpeaker = 9;
 
 int nbrLongBeep=0;
 int nbrShortBeep=0;
@@ -77,64 +77,81 @@ void setup()
   int val = 0;     // variable to store the read value
   int val1 = 0;     // variable to store the read value
 
- 
+
   //init Kalman filter
   KalmanInit();
   //Presure Sensor Initialisation
-  bmp.begin();
+  //bmp.begin();
+  //bmp.begin( BMP085_STANDARD);
+    //Low res should work better at high speed
+  bmp.begin( BMP085_ULTRALOWPOWER);
   //our drogue has not been fired
   apogeeHasFired=false;
   mainHasFired=false;
-  
+
   //Initialise the output pin
   pinMode(pinApogee, OUTPUT);
   pinMode(pinMain, OUTPUT);
   pinMode(pinSpeaker, OUTPUT);
- 
+
   pinMode(pinAltitude1, INPUT);
   pinMode(pinAltitude2, INPUT);
-  
+
   pinMode(pinApogeeContinuity, INPUT);
   pinMode(pinMainContinuity, INPUT);
   //Make sure that the output are turned off
   digitalWrite(pinApogee, LOW);
   digitalWrite(pinMain, LOW);
   digitalWrite(pinSpeaker, LOW);
+
+  //initialisation give the version of the altimeter
+  //One long beep per major number and One short beep per minor revision
+  //For example version 1.2 would be one long beep and 2 short beep
+  beepAltiVersion(1,5);
   
   //number of measures to do to detect Apogee
-  measures = 10;
+  measures = 5;
 
   //initialise the deployement altitude for the main 
   mainDeployAltitude = 100;
 
+  // On the Alti duo when you close the jumper you set it to 0
+  // val is the left jumper and val1 is the right jumper
 
   val = digitalRead(pinAltitude1); 
   val1 = digitalRead(pinAltitude2);  
   if(val == 0 && val1 ==0)
   {
-    mainDeployAltitude = 50;
+    mainDeployAltitude = 200;
   }
   if(val == 0 && val1 ==1)
   {
-    mainDeployAltitude = 100;
+    mainDeployAltitude = 150;
   }
   if(val == 1 && val1 ==0)
   {
-    mainDeployAltitude = 150;
+    mainDeployAltitude = 100;
   }
   if(val == 1 && val1 ==1)
   {
-    mainDeployAltitude = 200;
+    mainDeployAltitude = 50;
+  }
+
+  // let's do some dummy altitude reading
+  // to initialise the Kalman filter
+  for (int i=0; i<50; i++){
+    KalmanCalc(bmp.readAltitude());
   }
   //let's read the lauch site altitude
   long sum = 0;
   for (int i=0; i<10; i++){
     sum += KalmanCalc(bmp.readAltitude());
-    delay(500); }
+    delay(50); 
+  }
   initialAltitude = (sum / 10.0);
-  lastAltitude = initialAltitude; 
-  
-  liftoffAltitude = initialAltitude + 20;
+  lastAltitude = 0; 
+
+  liftoffAltitude =  20;
 }
 
 void loop()
@@ -146,7 +163,7 @@ void loop()
     continuityCheck(pinApogeeContinuity);
     continuityCheck(pinMainContinuity);
   }
-  
+
   //detect apogee
   if(currAltitude > liftoffAltitude)
   {
@@ -161,17 +178,18 @@ void loop()
         delay (2000);
         apogeeHasFired=true;
         digitalWrite(pinApogee, LOW);
-        apogeeAltitude = currAltitude;
+        //apogeeAltitude = currAltitude;
+        apogeeAltitude = lastAltitude;
       }  
     }
     else 
     {
       lastAltitude = currAltitude;
-      measures = 10;
+      measures = 5;
     } 
   }
 
- 
+
   if ((currAltitude  < mainDeployAltitude) && apogeeHasFired == true && mainHasFired==false)
   {
     // Deploy main chute  100m before landing...
@@ -184,13 +202,13 @@ void loop()
   if(apogeeHasFired == true && mainHasFired==true)
   {
     beginBeepSeq();
-    
+
     beepAltitude(apogeeAltitude);
 
     beginBeepSeq();
-    
+
     beepAltitude(mainAltitude);
-   
+
   }
 }
 
@@ -236,18 +254,18 @@ void beepAltitude(long altitude)
   }
 
   if (nbrLongBeep > 0)
-  for (i = 1; i <  nbrLongBeep +1 ; i++)
-  {
-    longBeep();
-    delay(50);
-  } 
+    for (i = 1; i <  nbrLongBeep +1 ; i++)
+    {
+      longBeep();
+      delay(50);
+    } 
 
   if (nbrShortBeep > 0)
-  for (i = 1; i <  nbrShortBeep +1 ; i++)
-  {
-    shortBeep();
-    delay(50);
-  } 
+    for (i = 1; i <  nbrShortBeep +1 ; i++)
+    {
+      shortBeep();
+      delay(50);
+    } 
 
   delay(5000);
 
@@ -295,38 +313,51 @@ void shortBeep()
 //KalmanInit() - Call before any iterations of KalmanCalc()
 void KalmanInit()
 {
-   kalman_q=4.0001;  //filter parameters, you can play around with them
-   kalman_r=.20001;  // but these values appear to be fairly optimal
+  kalman_q=4.0001;  //filter parameters, you can play around with them
+  kalman_r=.20001;  // but these values appear to be fairly optimal
 
-   kalman_x = 0;
-   kalman_p = 0;
-   kalman_x_temp = 0;
-   kalman_p_temp = 0;
-   
-   kalman_x_last = 0;
-   kalman_p_last = 0;
-   
+  kalman_x = 0;
+  kalman_p = 0;
+  kalman_x_temp = 0;
+  kalman_p_temp = 0;
+
+  kalman_x_last = 0;
+  kalman_p_last = 0;
+
 }
 
 //KalmanCalc() - Calculates new Kalman values from float value "altitude"
 // This will be the ASL altitude during the flight, and the AGL altitude during dumps
 float KalmanCalc (float altitude)
 {
-   
-   //Predict kalman_x_temp, kalman_p_temp
-   kalman_x_temp = kalman_x_last;
-   kalman_p_temp = kalman_p_last + kalman_r;
-   
-   //Update kalman values
-   kalman_k = (f_1/(kalman_p_temp + kalman_q)) * kalman_p_temp;
-   kalman_x = kalman_x_temp + (kalman_k * (altitude - kalman_x_temp));
-   kalman_p = (f_1 - kalman_k) * kalman_p_temp;
-   
-   //Save this state for next time
-   kalman_x_last = kalman_x;
-   kalman_p_last = kalman_p;
-   
-   //Assign current Kalman filtered altitude to working variables
-   //KAlt = kalman_x; //FLOAT Kalman-filtered altitude value
+
+  //Predict kalman_x_temp, kalman_p_temp
+  kalman_x_temp = kalman_x_last;
+  kalman_p_temp = kalman_p_last + kalman_r;
+
+  //Update kalman values
+  kalman_k = (f_1/(kalman_p_temp + kalman_q)) * kalman_p_temp;
+  kalman_x = kalman_x_temp + (kalman_k * (altitude - kalman_x_temp));
+  kalman_p = (f_1 - kalman_k) * kalman_p_temp;
+
+  //Save this state for next time
+  kalman_x_last = kalman_x;
+  kalman_p_last = kalman_p;
+
+  //Assign current Kalman filtered altitude to working variables
+  //KAlt = kalman_x; //FLOAT Kalman-filtered altitude value
   return kalman_x;
 }  
+
+void beepAltiVersion (int majorNbr, int minorNbr)
+{
+  int i;
+  for (i=0; i<majorNbr;i++)
+  {
+    longBeep();
+  }
+  for (i=0; i<minorNbr;i++)
+  {
+    shortBeep();
+  }  
+}
